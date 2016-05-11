689851d29a57b5589909075c524212a26a169cb5
mvyvip <mvyvip@sina.com>
2016-05-11 23:19:40 +0800

    完美无bug版本

---
+++ src/main/java/com/gogo/crm/web/controller/MainController.java
@@ -0,0 +1,31 @@
+package com.gogo.crm.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2016/5/11.
+ */
+@Controller
+@RequestMapping("/main")
+public class MainController {
+
+
+    /**
+     * 登录界面
+     * @return
+     */
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    /**
+     * 系统界面
+     * @return
+     */
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
